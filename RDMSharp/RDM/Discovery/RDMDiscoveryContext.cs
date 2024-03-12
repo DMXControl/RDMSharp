7cad674e25ee3321b512788053cf6cbf542f8764
Patrick Grote <patrick.grote@q-light.info>
2024-03-12 11:53:37 +0100

    CleanUp

---
+++ RDMSharp/RDM/Discovery/RDMDiscoveryContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RDMSharp
+{
+    internal class RDMDiscoveryContext
+    {
+        private readonly HashSet<RDMUID> _foundUids = new HashSet<RDMUID>();
+        private ulong _rangeToSearch = (ulong)RDMUID.Broadcast - 1;
+        private string _status;
+
+        private readonly IProgress<RDMDiscoveryStatus> _progress;
+
+        public RDMDiscoveryContext(IProgress<RDMDiscoveryStatus> progress = null)
+        {
+            this._progress = progress;
+        }
+
+        internal bool AlreadyFound(RDMUID uid) => _foundUids.Contains(uid);
+
+        internal void AddFound(RDMUID uid)
+        {
+            _foundUids.Add(uid);
+            _progress?.Report(GetStatus());
+        }
+
+        internal void AddFound(IEnumerable<RDMUID> uid)
+        {
+            _foundUids.UnionWith(uid);
+            _progress?.Report(GetStatus());
+        }
+
+        internal int FoundCount => _foundUids.Count;
+
+        internal IReadOnlyCollection<RDMUID> FoundUIDs => _foundUids.ToList();
+
+        internal void RemoveRange(ulong uidStart, ulong uidEnd)
+        {
+            var delta = uidEnd - uidStart + 1;
+            _rangeToSearch -= delta;
+            _progress?.Report(GetStatus());
+        }
+
+        internal string Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                _progress?.Report(GetStatus());
+            }
+        }
+
+        private RDMDiscoveryStatus GetStatus()
+        {
+            return new RDMDiscoveryStatus(FoundCount, _rangeToSearch, _status);
+        }
+    }
+}
---
+++ RDMSharp/RDM/Discovery/RDMDiscoveryStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace RDMSharp
+{
+    public readonly struct RDMDiscoveryStatus
+    {
+        public readonly int FoundDevices;
+        public readonly ulong RangeLeftToSearch;
+        public readonly string CurrentStatus;
+
+        public RDMDiscoveryStatus(in int found, in ulong left2search, in string status)
+        {
+            this.FoundDevices = found;
+            this.RangeLeftToSearch = left2search;
+            this.CurrentStatus = status;
+        }
+
+    }
+}
