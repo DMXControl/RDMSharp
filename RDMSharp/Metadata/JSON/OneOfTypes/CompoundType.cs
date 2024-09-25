5094b6a9f7b3d4418237d1de795abbfa571dd862
Patrick Grote <patrick.grote@q-light.info>
2024-09-25 21:23:25 +0200

    minor

--- RDMSharp/Metadata/JSON/OneOfTypes/CompoundType.cs
+++ RDMSharp/Metadata/JSON/OneOfTypes/CompoundType.cs
@@ -1,9 +1,8 @@
 ﻿using RDMSharp.RDM;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
-using System.Drawing;
 
 namespace RDMSharp.Metadata.JSON.OneOfTypes
 {
