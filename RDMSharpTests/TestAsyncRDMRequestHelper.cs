f95ea93d09bcc77959f226ea336ab2158317f1b9
Patrick Grote <patrick.grote@q-light.info>
2026-02-10 14:12:11 +0100

    Grrr

--- RDMSharpTests/TestAsyncRDMRequestHelper.cs
+++ RDMSharpTests/TestAsyncRDMRequestHelper.cs
@@ -156,7 +156,7 @@ public class TestAsyncRDMRequestHelper
     }
     private void validate(RDMMessage request, RequestResult result)
     {
-        NUnitString failMessage = $"Request: {request.ToString()} Response: {result.Response?.ToString()}";
+        NUnitString failMessage = $"Request: {request.ToString()} Response: {result.Response?.ToString()} Error: {result.Response?.NackReason.ToString() ?? result.Response?.ResponseType?.ToString()}";
 
         if (request.Command == ERDM_Command.SET_COMMAND && request.SubDevice.IsBroadcast)
         {
